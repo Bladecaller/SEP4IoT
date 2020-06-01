17958a1c6261e16504060fc9c0d35c337614cb51
Bladecaller <58396373+Bladecaller@users.noreply.github.com>
2020-06-01 11:13:59 +0300

    Add files via upload

---
+++ CO2Handler.c
@@ -0,0 +1,51 @@
+﻿
+/*
+ * CO2Handler.c
+ *
+ * Created: 23.5.2020 г. 10:49:34
+ *  Author: Borislav Aleksiev
+ */ 
+#include <stddef.h>
+#include <stdio.h>
+
+#include <ATMEGA_FreeRTOS.h>
+
+#include <lora_driver.h>
+#include <iled.h>
+#include "bits.h"
+#include "event_groups.h"
+	uint16_t CO2level;
+
+	
+void CO2_handler_task( void *pvParameters );
+void CO2Handler_create(){
+	//void mh_z19_create(e_com_port_t com_port,void(*mh_z19_call_back )(uint16_t ppm)); should make this work out here
+	xTaskCreate(
+	CO2_handler_task
+	,  (const portCHAR *)"CO2HandTask"  // A name just for humans
+	,  configMINIMAL_STACK_SIZE+200  // This stack size can be checked & adjusted by reading the Stack Highwater
+	,  NULL
+	,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+	,  NULL );
+	
+	
+}
+uint16_t getCO2lvl(){
+	mh_z19_get_co2_ppm(CO2level);
+	return CO2level;
+}
+
+void CO2_handler_task( void *pvParameters ){
+		mh_z19_create(ser_USART3,NULL);//Params are a matter of testing
+	for (;;) {
+		extern xCreatedEventGroup;
+		//xEventGroupSetBits(xCreatedEventGroup, BIT_0);
+		puts("co2task");
+		mh_z19_take_meassuring();
+		//printf("CO2 value: >%s<\n", lora_driver_sent_upload_message(true, &_uplink_payload));
+		printf("%s%i\n","Arduino measurement CO2: ",getCO2lvl());
+		vTaskDelay(1000);
+	}
+}
+
+//mh_z19_return_code_t
---
+++ HumidityTempHandler.c
@@ -0,0 +1,74 @@
+﻿
+/*
+ * CO2Handler.c
+ *
+ * Created: 23.5.2020 г. 10:49:34
+ *  Author: Borislav Aleksiev
+ */ 
+#include <stddef.h>
+#include <stdio.h>
+
+#include <ATMEGA_FreeRTOS.h>
+
+#include <lora_driver.h>
+#include <iled.h>
+#include "bits.h"
+#include "event_groups.h"
+	float templevel;
+	float humLevel;
+
+	
+void hum_temp_task( void *pvParameters );
+
+void Hum_Temp_Handler_create(){
+	hih8120Create(); 
+	xTaskCreate(
+	hum_temp_task
+	,  (const portCHAR *)"HumTempTask"  // A name just for humans
+	,  configMINIMAL_STACK_SIZE+200  // This stack size can be checked & adjusted by reading the Stack Highwater
+	,  NULL
+	,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+	,  NULL );
+	
+	
+}
+uint16_t get_hum_lvl(){
+
+	if ( hih8120IsReady()==true){
+	humLevel = hih8120GetHumidity();
+	}
+else{
+	vTaskDelay(2000);
+humLevel= hih8120GetHumidity();
+	}
+	printf("%s%i\n","Arduino humidity level: ",humLevel);
+	return humLevel;
+}
+
+
+int16_t get_temp_lvl(){
+if ( hih8120IsReady()==true){
+	templevel = hih8120GetTemperature();
+	}
+else{
+	vTaskDelay(2000);
+templevel= hih8120GetTemperature();
+	}
+	
+	printf("%s%i\n","Arduino temperature level: ",templevel);
+	return templevel;
+}
+void hum_temp_task( void *pvParameters ){
+
+		for (;;) {
+			extern xCreatedEventGroup;
+			//xEventGroupSetBits(xCreatedEventGroup, BIT_1);
+			puts("humidityTask");
+			hih8120Meassure();
+			
+			vTaskDelay(150);
+			get_temp_lvl();
+			get_hum_lvl();
+		}
+	
+}
